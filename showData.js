2bd53eabf17d62ad635d2577d966601e83ec5cb8
Patrick Gerspach <41807410+pgerspach@users.noreply.github.com>
2019-02-15 21:45:34 -0500

    Delete showData.js

--- showData.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("./recruitData.json", (err,data)=>{
-    const recruitData = JSON.parse(data);
-    const year = 2018;
-        console.log(recruitData[year]["Clemson"]);
-})
-
